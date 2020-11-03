e906ebb3b3c377a35b7a8afedfe9dc7de0d9376e
LAPTOP-M3T41IUI\沧海 <xxj199608@163.com>
2020-11-03 09:59:10 +0800

    996

---
+++ js对象.js
@@ -0,0 +1,22 @@
+
+// var o = { a: 1 };
+// Object.defineProperty(o, "b", { value: 2, writable: false, enumerable: false, configurable: true });
+// //a和b都是数据属性，但特征值变化了
+// Object.getOwnPropertyDescriptor(o, "a"); // {value: 1, writable: true, enumerable: true, configurable: true}
+// Object.getOwnPropertyDescriptor(o, "b"); // {value: 2, writable: false, enumerable: false, configurable: true}
+// o.b = 3;
+// console.log(o.b); // 2
+// Object.defineProperty(o, 'b', {
+//   value: 2,
+//   writable: true,
+//   enumerable: false,
+//   configurable: false
+// })
+var o = {
+  get a() {
+    return 1
+  }
+}
+console.log(Object.getOwnPropertyDescriptor(o));
+
+console.log(o.a);
